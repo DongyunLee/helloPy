16d3c3a1e4e6a489c337f3d9e7faf960f90e4e1d
Doylee <lidongyun@shuwang-tech.com>
2018-02-28 16:29:47 +0800

    init

---
+++ index.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+# 文件名：test.py
+
+# print 'Hello,Python';
+
+# if True:
+#     print "Answer"
+#     print "True"
+# else:
+#     print "Answer"
+#     # 没有严格缩进，在执行时会报错
+#     print "False"
+
+# total = item_one + \
+#         item_two + \
+#         item_three
+
+# days = ['Monday', 'Tuesday', 'Wednesday',
+#         'Thursday', 'Friday']
+'''
+
+word = 'word'
+sentence = "sentence"
+a = """另一个段落"""
+
+'''
+'''
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+'''
+
+"""
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+"""
+
+# raw_input("按下 enter 键退出，其他任意键显示...\n")
+
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+
+
