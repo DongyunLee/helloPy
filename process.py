6c9e21d4fb84bcb8489a743afd8486ba16042b86
Doylee <lidongyun@shuwang-tech.com>
2018-03-01 18:03:42 +0800

    py基础——流程控制

---
+++ process.py
@@ -0,0 +1,64 @@
+# -*- encoding:utf-8 -*-
+# 条件语句(0/false/null false;"0" true)
+# if-else
+"""
+if "0":
+    print "\"0\" is true"
+else:
+    print "\"0\" is false"
+"""
+
+# if-elif-else
+"""
+a = 1
+if a == 0:
+    print "a = 0"
+elif a == 1:
+    print "a = 1"
+else:
+    print "a is undefined"
+"""
+# 循环语句
+# while
+"""
+a = 3
+i = 0
+n = 2
+while i <= a:
+    i += 1
+    n **= 3
+else:
+    print i, n
+i = 3
+while i <= a: print "lalala"
+"""
+
+# for（相当于PHP的foreach()）
+"""
+for value in "love":
+    print value
+    
+diction = [
+    'a',
+    111,
+    "222"
+]
+
+for i in range(len(diction)):
+    print diction[i]
+    if()
+"""
+"""九九乘法表
+for i in range(1,10):
+    for j in range(1,10):
+        if i < j:
+            break
+        a =  str(i) + " x " + str(j) + " = " + str(i * j)
+        if j == i:
+            print a
+            continue
+        print a,
+"""
+
+# while
+#   for
